2ec0130b1096826d6bc2af79c641b8412c3286cf
gladee88er <124483686+gladee88er@users.noreply.github.com>
2023-03-11 16:25:21 -0500

    Update content_agitator.py

--- content_agitator.py
+++ content_agitator.py
@@ -1,40 +1,44 @@
+#  This new code changes the design of the program by defining a function scrape_arlicle that can be used to help with future expansion of functionality. 
+#  Also the ability to scrape multiple URL;s has been added.
+
 from newspaper import Article
-import nltk
 from textblob import TextBlob
-from bs4 import BeautifulSoup
-import requests
 
-print('Welcome to my news scraper bitches!')
+def scrape_article(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    title = article.title
+    link = article.url
+    authors = article.authors
+    date = article.publish_date
+    image = article.top_image
+    summary = article.summary
+    text = article.text
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
 
-# create an article object
-article = Article('https://blog.finxter.com/newspaper3k-a-python-library-for-fast-web-scraping/')
-article.download()
-article.parse()
-article.nlp()
-title = article.title
-link = article.url
-authors = article.authors
-date = article.publish_date
-image = article.top_image
-summary = article.summary
-text = article.text
-blob = TextBlob(text)
-polarity = blob.sentiment.polarity
+    print('**********************************')
+    print(f'Title: {title}')
+    print(f'Link: {link}')
+    print(f'Author: {authors[0]}')
+    print(f'Publish Date: {date}')
+    print(f'Top Image: {image}')
+    print(f'Summary: ')
+    print(summary)
+    print()
+    if polarity > 0:
+        print('The article is positive.')
+    elif polarity < 0:
+        print('The article is negative.')
+    else:
+        print('The article is neutral.')
+    print('**********************************')
 
-print('**********************************')
-print(f'Title: {title}')
-print(f'Link: {link}')
-print(f'Author: {authors[0]}')
-print(f'Publish Date: {date}')
-print(f'Top Image: {image}')
-print(f'Summary: ')
-print(summary)
-print()
-if polarity > 0:
-    print('The article is positive.')
-elif polarity < 0:
-    print('The article is negative.')
-else:
-    print('The article is neutral.')
-print('**********************************')
+urls = ['https://www.askpython.com/python-modules/nltk-punkt', 'https://blog.finxter.com/newspaper3k-a-python-library-for-fast-web-scraping/']
+
+for url in urls:
+    scrape_article(url)
+
 
